404be371270e3ac604c69a6bf9dba95fd7b59fba
kenvez <kenvezdev@proton.me>
2024-08-20 21:35:20 +0200

    started top sellers section

---
+++ components/CreatorCard.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import images from '../assets';
+
+const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => {
+  return (
+    <div className='min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-3 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4'>
+      <div>
+        <p>{rank}</p>
+      </div>
+      <div className='my-2 flex justify-center'>
+        <div className='relative w-20 h-20 minlg:w-28 minlg:h-28'>
+          <Image src={creatorImage} layout='fit' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatorCard;
