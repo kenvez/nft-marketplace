f5b18a8df87976d43badefc7a1956d8cede4562c
kenvez <kenvezdev@proton.me>
2024-08-20 21:07:15 +0200

    finished banner

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import { Banner } from '../components';
 const Page = () => (
   <div className='flex justify-center sm:px-6 p-12'>
     <div className='w-full minmd:w-4/5'>
-      <Banner parentStyles='justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl' />
+      <Banner
+        name='Discover, collect, and sell extraordinary NFTs'
+        childStyles='md:text-4xl sm:text-2xl xs:text-xl text-left'
+        parentStyles='justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl'
+      />
     </div>
   </div>
 );
