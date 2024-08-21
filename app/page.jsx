3c60a992d131d6a95f3b585c7f5e283e0835a2ca
kenvez <kenvezdev@proton.me>
2024-08-21 20:42:52 +0200

    created nft card component

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useState, useEffect, useRef } from 'react';
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import { makeId } from '../utils/makeId';
 import { useTheme } from 'next-themes';
 import images from '../assets';
@@ -59,7 +59,7 @@ const Home = () => {
               className='flex flex-row w-max overflow-x-scroll no-scrollbar select-none'
               ref={scrollRef}
             >
-              {[6, 7, 8, 9, 10].map((i) => (
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
                 <CreatorCard
                   key={`creator-${i}`}
                   rank={i}
@@ -97,6 +97,28 @@ const Home = () => {
                 </>
               )}
             </div>
+          </div>
+        </div>
+        <div className='mt-10'>
+          <div className='flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start'>
+            <h1 className=' flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4'>
+              Top nfts
+            </h1>
+            <div>SearchBar</div>
+          </div>
+          <div className='mt-3 w-full flex flex-wrap justify-start md:justify-center'>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nift NFT ${i}`,
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool nft on sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
