8c98284327adf516edb754ba25f470e25f496794
kenvez <kenvezdev@proton.me>
2024-08-21 21:53:29 +0200

    created nft cards

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -18,7 +18,17 @@ const NFTCard = ({ nft }) => (
           alt='nft01'
         />
       </div>
-      {nft.name}
+      <div className='mt-3 flex flex-col'>
+        <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl'>
+          {nft.name}
+        </p>
+        <div className='flexBetween mt-3 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3'>
+          <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg'>
+            {nft.price} <span className='normal'></span>ETH
+          </p>
+          <p>{nft.seller}</p>
+        </div>
+      </div>
     </div>
   </Link>
 );
