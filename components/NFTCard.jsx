347448511775bbd9486a48807aca31b4958d12bd
kenvez <kenvezdev@proton.me>
2024-08-22 22:28:58 +0200

    created top sellers section

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -22,11 +22,13 @@ const NFTCard = ({ nft }) => (
         <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl'>
           {nft.name}
         </p>
-        <div className='flexBetween mt-3 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3'>
+        <div className='flexBetween mt-1 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3'>
           <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg'>
             {nft.price} <span className='normal'></span>ETH
           </p>
-          <p>{nft.seller}</p>
+          <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg'>
+            {nft.seller}
+          </p>
         </div>
       </div>
     </div>
