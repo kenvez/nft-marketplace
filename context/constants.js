2ba1502740b35a1cf6d4666b6a9f9a92325034bc
kenvez <kenvezdev@proton.me>
2024-08-26 22:10:15 +0200

    finished smart contract

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
-import marketAbi from './NFTMarketplace.json';
+import market from './NFTMarketplace.json';
 
 export const MarketAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 export const MarketAddressABI = market.abi;
